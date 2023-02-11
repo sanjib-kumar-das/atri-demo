334ab44874cfbed104209dd1c0a8a3436e895fde
sanjib-kumar-das <sanjibdas.23012002@gmail.com>
2023-02-11 19:44:16 +0530

    first commit

---
+++ atri_app/app/src/page-cbs/Home.js
@@ -0,0 +1,30 @@
+import { useCallback } from "react";
+import { callbackFactory } from "../utils/callbackFactory";
+export function useFlex1Cb() {
+	const onClick = useCallback(callbackFactory("Flex1", "Home", "/", "onClick", 
+			{
+  "handlers": [],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useTextBox1Cb() {
+	const onClick = useCallback(callbackFactory("TextBox1", "Home", "/", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
---
+++ atri_app/app/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import { useLayoutEffect, useEffect } from "react";
+import useStore, { updateStoreStateFromController } from "../hooks/useStore";
+import useIoStore from "../hooks/useIoStore";
+import { useNavigate, useLocation } from "react-router-dom";
+import { subscribeInternalNavigation } from "../utils/navigate";
+import {fetchPageProps} from "../utils/fetchPageProps"
+import { Flex } from "@atrilabs/react-component-manifests/src/manifests/Flex/Flex.tsx";
+import { TextBox } from "@atrilabs/react-component-manifests/src/manifests/TextBox/TextBox.tsx";
+import { useFlex1Cb, useTextBox1Cb } from "../page-cbs/Home";
+import "../page-css/Home.css";
+import "../custom/Home";
+
+export default function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const unsub = subscribeInternalNavigation((url) => {
+      navigate(url);
+    });
+    return unsub;
+  }, [navigate]);
+
+  const location = useLocation();
+  useLayoutEffect(()=>{
+    fetchPageProps(location.pathname, location.search).then((res)=>{
+      updateStoreStateFromController(res.pageName, res.pageState)
+    })
+  }, [location])
+
+  const Flex1Props = useStore((state)=>state["Home"]["Flex1"]);
+const Flex1IoProps = useIoStore((state)=>state["Home"]["Flex1"]);
+const Flex1Cb = useFlex1Cb()
+const TextBox1Props = useStore((state)=>state["Home"]["TextBox1"]);
+const TextBox1IoProps = useIoStore((state)=>state["Home"]["TextBox1"]);
+const TextBox1Cb = useTextBox1Cb()
+
+  return (<>
+  <Flex className="p-Home Flex1 bpt" {...Flex1Props} {...Flex1Cb} {...Flex1IoProps}>
+<TextBox className="p-Home TextBox1 bpt" {...TextBox1Props} {...TextBox1Cb} {...TextBox1IoProps}/>
+</Flex>
+  </>);
+}
